1f3f520339a450c390376a455ad4d0cdf8254f3a
Walid Yousfi <walidyousfipro@gmail.com>
2022-11-16 11:01:05 +0100

    collapseTitle size fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
-import "../styles/Card.css"
+import "../styles/Card.css";
 import { useNavigate } from "react-router-dom";
 
-const Card = ({children, src, lodgingId}) => {
-   let navigate = useNavigate(); 
-    return ( 
-        <div onClick={() => navigate("/logement/" + lodgingId ) } className="card" style={!src ? {background: "linear-gradient(180deg, rgba(249, 105, 106, 1) 0%, rgba(129, 50, 50, 1) 100%)"} : {backgroundImage:"url("+ src + ")"}}>
-           <p className="cardTitle">{children}</p>
-         </div>
-     );
-}
- 
+const Card = ({ children, src, lodgingId }) => {
+  let navigate = useNavigate();
+  return (
+    <div onClick={() => navigate("/logement/" + lodgingId)} className="card" style={!src ? { background: "linear-gradient(180deg, rgba(249, 105, 106, 1) 0%, rgba(129, 50, 50, 1) 100%)" } : { backgroundImage: "url(" + src + ")" }}>
+      <p className="cardTitle">{children}</p>
+    </div>
+  );
+};
+
 export default Card;
